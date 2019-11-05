8127045c8995f62b3067a9081e021efd251b8e76
Dzhem <dzhem.aptula@gmail.com>
2019-11-05 16:25:11 +0100

    Kinda init I guess

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCHello.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from index in HelloController";
+        }
+
+        public string About(int id)
+        {
+            return $"Hello from About with id {id}";
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCHello.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from index in homecontroller";
+        }
+        public string About()
+        {
+            return "This is the about page";
+        }
+        public string Contact()
+        {
+            return "This is the contact page";
+        }
+    }
+}
